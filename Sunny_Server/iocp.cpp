03359814b238eb4ec1af5c06ca5ad7ed2fb04c64
GodHongIl <adunstudio@daum.net>
2018-04-12 15:17:12 +0900

    게임 서버 프로그래밍(IOCP)
    - iocp.cpp
    - protocol.cpp

---
+++ Sunny_Server/iocp.cpp
@@ -0,0 +1,304 @@
+#define WIN32_LEAN_AND_MEAN  
+#define INITGUID
+
+#include <WinSock2.h>
+#include <windows.h>   // include important windows stuff
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include "protocol.h"
+
+#include <thread>
+#include <vector>
+#include <array>
+#include <iostream>
+using namespace std; 
+
+HANDLE gh_iocp;
+struct EXOVER {
+	WSAOVERLAPPED m_over;
+	char m_iobuf[MAX_BUFF_SIZE];
+	WSABUF m_wsabuf;
+	bool is_recv;
+};
+
+class Client {
+public:
+	SOCKET m_s;
+	bool m_isconnected;
+	int m_x;
+	int m_y;
+	EXOVER m_rxover;
+	int m_packet_size;  // 지금 조립하고 있는 패킷의 크기
+	int	m_prev_packet_size; // 지난번 recv에서 완성되지 않아서 저장해 놓은 패킷의 앞부분의 크기
+	char m_packet[MAX_PACKET_SIZE];
+
+	Client()
+	{
+		m_isconnected = false;
+		m_x = 4;
+		m_y = 4;
+
+		ZeroMemory(&m_rxover.m_over, sizeof(WSAOVERLAPPED));
+		m_rxover.m_wsabuf.buf = m_rxover.m_iobuf;
+		m_rxover.m_wsabuf.len = sizeof(m_rxover.m_wsabuf.buf);
+		m_rxover.is_recv = true;
+		m_prev_packet_size = 0;
+	}
+};
+
+array <Client, MAX_USER> g_clients;
+
+void error_display(const char *msg, int err_no)
+{
+	WCHAR *lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, err_no,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	std::cout << msg;
+	std::wcout << L"  에러" << lpMsgBuf << std::endl;
+	LocalFree(lpMsgBuf);
+	while (true);
+}
+
+void ErrorDisplay(const char * location)
+{
+	error_display(location, WSAGetLastError());
+}
+
+void initialize()
+{
+	gh_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0); // 의미없는 파라메터, 마지막은 알아서 쓰레드를 만들어준다.
+	std::wcout.imbue(std::locale("korean"));
+
+	WSADATA	wsadata;
+	WSAStartup(MAKEWORD(2, 2), &wsadata);
+}
+
+void StartRecv(int id)
+{
+	unsigned long r_flag = 0;
+	ZeroMemory(&g_clients[id].m_rxover.m_over, sizeof(WSAOVERLAPPED));
+	int ret = WSARecv(g_clients[id].m_s, &g_clients[id].m_rxover.m_wsabuf, 1,
+		NULL, &r_flag, &g_clients[id].m_rxover.m_over, NULL);
+	if (0 != ret) {
+		int err_no = WSAGetLastError();
+		if (WSA_IO_PENDING != err_no) error_display("Recv Error", err_no);
+	}
+}
+
+void SendPacket(int id, void *ptr)
+{
+	char *packet = reinterpret_cast<char *>(ptr);
+	EXOVER *s_over = new EXOVER;
+	s_over->is_recv = false;
+	memcpy(s_over->m_iobuf, packet, packet[0]);
+	s_over->m_wsabuf.buf = s_over->m_iobuf;
+	s_over->m_wsabuf.len = s_over->m_iobuf[0];
+	ZeroMemory(&s_over->m_over, sizeof(WSAOVERLAPPED));
+	int res = WSASend(g_clients[id].m_s, &s_over->m_wsabuf, 1, NULL, 0,
+		&s_over->m_over, NULL);
+	if (0 != res) {
+		int err_no = WSAGetLastError();
+		if (WSA_IO_PENDING != err_no) error_display("Send Error! ", err_no);
+	}
+}
+
+void ProcessPacket(int id, char *packet)
+{
+	int x = g_clients[id].m_x;
+	int y = g_clients[id].m_y;
+	switch (packet[1])
+	{
+	case CS_UP: if (y > 0) y--; break;
+	case CS_DOWN: if (y < BOARD_HEIGHT - 1) y++; break;
+	case CS_LEFT: if (x > 0) x--; break;
+	case CS_RIGHT: if (x < BOARD_WIDTH - 1) x++; break;
+	default:
+		cout << "Unkown Packet Type from Client [" << id << "]\n";
+		return;
+	}
+	g_clients[id].m_x = x;
+	g_clients[id].m_y = y;
+
+	sc_packet_pos pos_packet;
+
+	pos_packet.id = id;
+	pos_packet.size = sizeof(sc_packet_pos);
+	pos_packet.type = SC_POS;
+	pos_packet.x = x;
+	pos_packet.y = y;
+
+	for (int i = 0; i < MAX_USER; ++i)
+	{
+		if(g_clients[i].m_isconnected == true)
+			SendPacket(id, &pos_packet);
+	}
+}
+
+void DisconnectPlayer(int id)
+{
+	sc_packet_remove_player p;
+	p.id = id;
+	p.size = sizeof(p);
+	p.type = SC_REMOVE_PLAYER;
+
+	for (int i = 0; i < MAX_USER; ++i)
+	{
+		if (g_clients[i].m_isconnected == false) continue;
+		if (id == i) continue;
+		SendPacket(i, &p);
+	}
+		
+	closesocket(g_clients[id].m_s);
+	g_clients[id].m_isconnected = false;
+}
+
+void worker_thread()
+{
+	while (true)
+	{
+		unsigned long io_size;
+		unsigned long long iocp_key; // 64 비트 integer , 우리가 64비트로 컴파일해서 64비트
+		WSAOVERLAPPED *over;
+		BOOL ret = GetQueuedCompletionStatus(gh_iocp, &io_size, &iocp_key, &over, INFINITE);
+		int key = static_cast<int>(iocp_key);
+		cout << "WT::Network I/O with Client [" << key << "]\n";
+		if (FALSE == ret) {
+			cout << "Error in GQCS\n";
+			DisconnectPlayer(key);
+			continue;
+		}
+		if (0 == io_size) {
+			DisconnectPlayer(key);
+			continue;
+		}
+
+		EXOVER *p_over = reinterpret_cast<EXOVER *>(over);
+		if (true == p_over->is_recv) {
+			cout << "WT:Packet From Client [" << key << "]\n";
+			int work_size = io_size;
+			char *wptr = p_over->m_iobuf;
+			while (0 < work_size) {
+				int p_size;
+				if (0 != g_clients[key].m_packet_size)
+					p_size = g_clients[key].m_packet_size;
+				else {
+					p_size = wptr[0];
+					g_clients[key].m_packet_size = p_size;
+				}
+				int need_size = p_size - g_clients[key].m_prev_packet_size;
+				if (need_size <= work_size) {
+					memcpy(g_clients[key].m_packet
+						+ g_clients[key].m_prev_packet_size, wptr, need_size);
+					ProcessPacket(key, g_clients[key].m_packet);
+					g_clients[key].m_prev_packet_size = 0;
+					g_clients[key].m_packet_size = 0;
+					work_size -= need_size;
+					wptr += need_size;
+				}
+				else {
+					memcpy(g_clients[key].m_packet + g_clients[key].m_prev_packet_size, wptr, work_size);
+					g_clients[key].m_prev_packet_size += work_size;
+					work_size = -work_size;
+					wptr += work_size;
+				}
+			}
+			StartRecv(key);
+		}
+		else {  // Send 후처리
+			cout << "WT:A packet was sent to Client[" << key << "]\n";
+			delete p_over;
+		}
+	}
+}
+
+void accept_thread()	//새로 접속해 오는 클라이언트를 IOCP로 넘기는 역할
+{
+	SOCKET s = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+
+	SOCKADDR_IN bind_addr;
+	ZeroMemory(&bind_addr, sizeof(SOCKADDR_IN));
+	bind_addr.sin_family = AF_INET;
+	bind_addr.sin_port = htons(MY_SERVER_PORT);
+	bind_addr.sin_addr.s_addr = INADDR_ANY;	// 0.0.0.0  아무대서나 오는 것을 다 받겠다.
+
+	::bind(s, reinterpret_cast<sockaddr *>(&bind_addr), sizeof(bind_addr));
+	listen(s, 1000);
+
+	while (true)
+	{
+		SOCKADDR_IN c_addr;
+		ZeroMemory(&c_addr, sizeof(SOCKADDR_IN));
+		c_addr.sin_family = AF_INET;
+		c_addr.sin_port = htons(MY_SERVER_PORT);
+		c_addr.sin_addr.s_addr = INADDR_ANY;	// 0.0.0.0  아무대서나 오는 것을 다 받겠다.
+		int addr_size = sizeof(sockaddr);
+
+		SOCKET cs = WSAAccept(s, reinterpret_cast<sockaddr *>(&c_addr), &addr_size, NULL, NULL);
+		if (INVALID_SOCKET == cs) {
+			ErrorDisplay("In Accept Thread:WSAAccept()");
+			continue;
+		}
+		cout << "New Client Connected!\n";
+		int id = -1;
+		for (int i = 0; i < MAX_USER; ++i)
+			if (false == g_clients[i].m_isconnected) {
+				id = i;
+				break;
+			}
+		if (-1 == id) {
+			cout << "MAX USER Exceeded\n";
+			continue;
+		}
+		cout << "ID of new Client is [" << id << "]";
+		g_clients[id].m_s = cs;
+		g_clients[id].m_packet_size = 0;
+		g_clients[id].m_prev_packet_size = 0;
+		g_clients[id].m_x = 4;
+		g_clients[id].m_y = 4;
+
+		CreateIoCompletionPort(reinterpret_cast<HANDLE>(cs), gh_iocp, id, 0);
+		g_clients[id].m_isconnected = true;
+		StartRecv(id);
+
+		sc_packet_put_player p;
+		p.id = id;
+		p.size = sizeof(p);
+		p.type = SC_PUT_PLAYER;
+		p.x = g_clients[id].m_x;
+		p.y = g_clients[id].m_y;
+
+		for (int i = 0; i < MAX_USER; ++i)
+			if (g_clients[i].m_isconnected == true)
+				SendPacket(i, &p);
+
+		for (int i = 0; i < MAX_USER; ++i)
+		{
+			if (g_clients[i].m_isconnected == false) continue;
+			if (i == id) continue;
+
+			p.id = i;
+			p.x = g_clients[i].m_x;
+			p.y = g_clients[i].m_y;
+			SendPacket(id, &p);
+
+		}
+	}
+}
+
+int main()
+{
+	vector <thread> w_threads;
+	initialize();
+	//CreateWorkerThreads();	// 쓰레드 조인까지 이 안에서 해주어야 한다. 전역변수 해서 관리를 해야 함. 전역변수 만드는 것은
+	// 좋은 방법이 아님.
+	for (int i = 0; i < 4; ++i) w_threads.push_back(thread{ worker_thread }); // 4인 이유는 쿼드코어 CPU 라서
+																			  //CreateAcceptThreads();
+	thread a_thread{ accept_thread };
+	for (auto& th : w_threads) th.join();
+	a_thread.join();
+}
